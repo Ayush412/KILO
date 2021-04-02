d06452d749628994b63d0c9448bed7ba5d1bfde2
Dhir Thacker <dhirthacker7@gmail.com>
2021-04-02 22:53:36 +0530

    Profile and comments repo changed

--- lib/repository/comments_repo.dart
+++ lib/repository/comments_repo.dart
@@ -3,60 +3,77 @@ import 'package:kilo/bloc/comments_bloc.dart';
 import 'package:date_format/date_format.dart';
 import 'package:kilo/bloc/login/login_bloc.dart';
 
-class CommentsRepo{
-
+class CommentsRepo {
   List<DocumentSnapshot> comments = [];
   DocumentSnapshot lastDoc;
   bool moreLoading = false;
   bool moreDocsLeft = true;
   String date = formatDate(DateTime.now(), [yyyy, '-', mm, '-', dd]);
 
-  getComments(String docID) async{
+  getComments(String docID) async {
     comments = [];
     lastDoc = null;
-    QuerySnapshot qs = await FirebaseFirestore.instance.collection('posts/$docID/comments')
-    .orderBy('Date', descending: true)
-    .limit(10).get();
+    QuerySnapshot qs = await FirebaseFirestore.instance
+        .collection('posts/$docID/comments')
+        .orderBy('Date', descending: true)
+        .limit(10)
+        .get();
     comments = qs.docs;
-    if(comments.length == 0){
+    if (comments.length == 0) {
       return null;
     }
     commentsBloc.commentsIn.add(comments);
   }
 
-  getNextComments(String docID) async{
-    if(moreDocsLeft == false){
+  getNextComments(String docID) async {
+    if (moreDocsLeft == false) {
       return;
     }
-    if(moreLoading == true){
+    if (moreLoading == true) {
       return;
     }
     moreLoading = true;
-    QuerySnapshot qs = await FirebaseFirestore.instance.collection('posts/$docID/comments')
-    .orderBy('Date', descending: true)
-    .startAfter([lastDoc.data()['Date']])
-    .limit(2).get();
-    if(qs.docs.length == 0){
+    QuerySnapshot qs = await FirebaseFirestore.instance
+        .collection('posts/$docID/comments')
+        .orderBy('Date', descending: true)
+        .startAfter([lastDoc.data()['Date']])
+        .limit(2)
+        .get();
+    if (qs.docs.length == 0) {
       moreDocsLeft = false;
       return;
     }
     comments.addAll(qs.docs);
-    lastDoc = qs.docs[qs.docs.length-1];
+    lastDoc = qs.docs[qs.docs.length - 1];
     moreLoading = false;
     commentsBloc.commentsIn.add(comments);
   }
 
-  postComment(String text, String docID) async{
-    await FirebaseFirestore.instance.collection('posts/$docID/comments').doc().set({
+  postComment(String text, String docID) async {
+    await FirebaseFirestore.instance
+        .collection('posts/$docID/comments')
+        .doc()
+        .set({
       'Name': loginBloc.userMap['Name'],
       'Text': text,
       'Date': date,
       'email': loginBloc.emailID
     });
+    await FirebaseFirestore.instance
+        .collection('posts')
+        .doc(docID)
+        .update({'Comments': FieldValue.increment(1)});
   }
 
-  deleteComment(String postID, String commentID) async{
-    await FirebaseFirestore.instance.collection('posts/$postID/comments').doc(commentID).delete();
+  deleteComment(String postID, String commentID) async {
+    await FirebaseFirestore.instance
+        .collection('posts/$postID/comments')
+        .doc(commentID)
+        .delete();
+    await FirebaseFirestore.instance
+        .collection('posts')
+        .doc(postID)
+        .update({'Comments': FieldValue.increment(-1)});
   }
 }
 
--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -16,243 +16,250 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-            child: Center(
-                child: Column(
-      children: <Widget>[
-        Container(
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [Colors.orangeAccent, Colors.deepOrange])),
-            child: Container(
-              width: double.infinity,
-              height: 240.0,
+        backgroundColor: Colors.grey[900],
+        body: SafeArea(
+          child: SingleChildScrollView(
               child: Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(
-                        "",
+                  child: Column(
+            children: <Widget>[
+              Container(
+                  // decoration: BoxDecoration(
+                  // gradient: LinearGradient(
+                  //     begin: Alignment.topCenter, end: Alignment.bottomCenter,
+                  // colors: [Colors.white])),
+                  // colors: [Colors.orangeAccent, Colors.deepOrange])),
+                  child: Container(
+                width: double.infinity,
+                height: 160.0,
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          "https://firebasestorage.googleapis.com/v0/b/kilo-a719c.appspot.com/o/puppy.jpeg?alt=media&token=6946cd13-6d78-4d98-a997-caa6ac1aa4d9",
+                        ),
+                        radius: 50.0,
                       ),
-                      radius: 50.0,
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Text(
-                      "${loginBloc.userMap['Name']}​​",
-                      style: TextStyle(
-                        fontSize: 22.0,
-                        color: Colors.white,
+                      SizedBox(
+                        height: 10.0,
                       ),
-                    ),
-                    SizedBox(
-                      height: 5.0,
-                    ),
-                  ],
-                ),
-              ),
-            )),
-        SizedBox(
-          height: 10.0,
-        ),
-        Card(
-            margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-            clipBehavior: Clip.antiAlias,
-            color: Colors.white,
-            elevation: 5.0,
-            child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 22.0),
-                child: Row(children: <Widget>[
-                  Expanded(
-                    child: Column(
-                      children: <Widget>[
-                        Text(
-                          "Age",
-                          style: TextStyle(
-                            color: Colors.deepOrangeAccent[700],
-                            fontSize: 22.0,
-                            fontWeight: FontWeight.bold,
-                          ),
+                      Text(
+                        "${loginBloc.userMap['Name']}​​",
+                        style: TextStyle(
+                          color: Colors.white,
+                          // color: Colors.deepOrangeAccent[700],
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.bold,
                         ),
-                        SizedBox(
-                          height: 2.0,
-                        ),
-                        Text(
-                          "${loginBloc.userMap['Age']}​​",
-                          style: TextStyle(
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.deepOrange,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ]))),
-        SizedBox(
-          height: 10.0,
-        ),
-        Card(
-            margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-            clipBehavior: Clip.antiAlias,
-            color: Colors.white,
-            elevation: 5.0,
-            child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 22.0),
-                child: Row(children: <Widget>[
-                  Expanded(
-                    child: Column(
-                      children: <Widget>[
-                        Text(
-                          "Weight (kg)",
-                          style: TextStyle(
-                            color: Colors.deepOrangeAccent[700],
-                            fontSize: 22.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 2.0,
-                        ),
-                        Text(
-                          "${loginBloc.userMap['Weight']}",
-                          style: TextStyle(
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.deepOrange,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ]))),
-        SizedBox(
-          height: 10.0,
-        ),
-        Card(
-            margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-            clipBehavior: Clip.antiAlias,
-            color: Colors.white,
-            elevation: 5.0,
-            child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 22.0),
-                child: Row(children: <Widget>[
-                  Expanded(
-                    child: Column(
-                      children: <Widget>[
-                        Text(
-                          "Height (cm)",
-                          style: TextStyle(
-                            color: Colors.deepOrangeAccent[700],
-                            fontSize: 22.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 2.0,
-                        ),
-                        Text(
-                          "${loginBloc.userMap['Height']}",
-                          style: TextStyle(
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.deepOrange,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ]))),
-        SizedBox(
-          height: 10.0,
-        ),
-        Card(
-            margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-            clipBehavior: Clip.antiAlias,
-            color: Colors.white,
-            elevation: 5.0,
-            child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 22.0),
-                child: Row(children: <Widget>[
-                  Expanded(
-                    child: Column(
-                      children: <Widget>[
-                        Text(
-                          "BMI",
-                          style: TextStyle(
-                            color: Colors.deepOrangeAccent[700],
-                            fontSize: 22.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 2.0,
-                        ),
-                        Text(
-                          "${loginBloc.userMap['BMI']}",
-                          style: TextStyle(
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.deepOrange,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ]))),
-        SizedBox(
-          height: 30.0,
-        ),
-        Container(
-          width: 200.00,
-          child: RaisedButton(
-              onPressed: () {},
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(80.0)),
-              elevation: 0.0,
-              padding: EdgeInsets.all(0.0),
-              child: Ink(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.center,
-                      end: Alignment.centerRight,
-                      colors: [
-                        Colors.deepOrangeAccent,
-                        Colors.deepOrangeAccent
-                      ]),
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                child: Container(
-                  constraints: BoxConstraints(maxWidth: 300.0, minHeight: 50.0),
-                  alignment: Alignment.center,
-                  child: Text(
-                    "Badges",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 26.0,
-                        fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 5.0,
+                      ),
+                    ],
                   ),
                 ),
               )),
-        ),
-        Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 80),
-            child: RaisedButton(
-                color: Colors.white,
-                elevation: 1,
-                onPressed: () => logOut(),
-                child:
-                    Text("Logout", style: TextStyle(color: Colors.deepOrange))))
-      ],
-    ))));
+              SizedBox(
+                height: 10.0,
+              ),
+              Card(
+                  margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                  clipBehavior: Clip.antiAlias,
+                  color: Colors.grey[800],
+                  elevation: 5.0,
+                  child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8.0, vertical: 22.0),
+                      child: Row(children: <Widget>[
+                        Expanded(
+                          child: Column(
+                            children: <Widget>[
+                              Text(
+                                "Age",
+                                style: TextStyle(
+                                  color: Colors.orange[400],
+                                  fontSize: 22.0,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 2.0,
+                              ),
+                              Text(
+                                "${loginBloc.userMap['Age']}​​",
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.orange[400],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ]))),
+              SizedBox(
+                height: 10.0,
+              ),
+              Card(
+                  margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                  clipBehavior: Clip.antiAlias,
+                  color: Colors.grey[800],
+                  elevation: 5.0,
+                  child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8.0, vertical: 22.0),
+                      child: Row(children: <Widget>[
+                        Expanded(
+                          child: Column(
+                            children: <Widget>[
+                              Text(
+                                "Weight (kg)",
+                                style: TextStyle(
+                                  color: Colors.orange[400],
+                                  fontSize: 22.0,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 2.0,
+                              ),
+                              Text(
+                                "${loginBloc.userMap['Weight']}",
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.orange[400],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ]))),
+              SizedBox(
+                height: 10.0,
+              ),
+              Card(
+                  margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                  clipBehavior: Clip.antiAlias,
+                  color: Colors.grey[800],
+                  elevation: 5.0,
+                  child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8.0, vertical: 22.0),
+                      child: Row(children: <Widget>[
+                        Expanded(
+                          child: Column(
+                            children: <Widget>[
+                              Text(
+                                "Height (cm)",
+                                style: TextStyle(
+                                  color: Colors.orange[400],
+                                  fontSize: 22.0,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 2.0,
+                              ),
+                              Text(
+                                "${loginBloc.userMap['Height']}",
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.orange[400],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ]))),
+              SizedBox(
+                height: 10.0,
+              ),
+              Card(
+                  margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                  clipBehavior: Clip.antiAlias,
+                  color: Colors.grey[800],
+                  elevation: 5.0,
+                  child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8.0, vertical: 22.0),
+                      child: Row(children: <Widget>[
+                        Expanded(
+                          child: Column(
+                            children: <Widget>[
+                              Text(
+                                "BMI",
+                                style: TextStyle(
+                                  color: Colors.orange[400],
+                                  fontSize: 22.0,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 2.0,
+                              ),
+                              Text(
+                                "${loginBloc.userMap['BMI']}",
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.orange[400],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ]))),
+              SizedBox(
+                height: 30.0,
+              ),
+              Container(
+                width: 200.00,
+                child: RaisedButton(
+                    onPressed: () {},
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(80.0)),
+                    elevation: 0.0,
+                    padding: EdgeInsets.all(0.0),
+                    child: Ink(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.center,
+                            end: Alignment.centerRight,
+                            colors: [
+                              Colors.deepOrangeAccent,
+                              Colors.deepOrangeAccent
+                            ]),
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      child: Container(
+                        constraints:
+                            BoxConstraints(maxWidth: 300.0, minHeight: 50.0),
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Badges",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 26.0,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    )),
+              ),
+              Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 80),
+                  child: RaisedButton(
+                      color: Colors.white,
+                      elevation: 1,
+                      onPressed: () => logOut(),
+                      child: Text("Logout",
+                          style: TextStyle(color: Colors.deepOrange))))
+            ],
+          ))),
+        ));
   }
 }
